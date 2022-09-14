5f0a624f5e85b5cc9ba6df4d0c686a94ce2f7e81
Mrsuper125 <Mrsuper1221@gmail.com>
2022-09-14 19:10:19 +0300

    Доделал вторую практику

--- Практика 2/Program.cs
+++ Практика 2/Program.cs
@@ -10,6 +10,11 @@ namespace Практика_2
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(Math.Pow(a + b, 1.0/3) + Math.Sin(a / 4) + Math.Sqrt((a + b + 4) / 2) + Math.Pow(c, 2));
         }
     }
 }
