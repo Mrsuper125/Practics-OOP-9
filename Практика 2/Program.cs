6c3e2a1ce86c78c3e52a272a3a038c97f72bcd6e
School PC <sash23546@gmail.com>
2022-09-19 13:12:44 +0300

    Finish practic 2 and show to teacher

--- Практика 2/Program.cs
+++ Практика 2/Program.cs
@@ -13,8 +13,11 @@ namespace Практика_2
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
             int c = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine(Math.Pow(c + b, 1.0/3.0) + Math.Sin(Convert.ToDouble(a) / 4.0) + Math.Sqrt((a + b + 4) / 2) + Math.Pow(c, 2));
+
+            double res = Math.Pow(c + b, 1.0 / 3.0) + Math.Sin(Convert.ToDouble(a) / 4.0) + Math.Sqrt((a + b + 4) / 2) + Math.Pow(c, 2);
+            Console.WriteLine($"{res:f2}");
+            Console.ReadKey();
+
         }
     }
 }
