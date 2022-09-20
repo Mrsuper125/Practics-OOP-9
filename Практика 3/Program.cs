8aa02253ce6a59ebea6cab8c7d28dc5e9f70ea8b
School PC <sash23546@gmail.com>
2022-09-20 11:05:35 +0300

    Практика 3

---
+++ Практика 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 0) Console.WriteLine("Не бей по своим!");
+            else if (a < 28) Console.WriteLine("НЕДОЛЕТ");
+            else if (a < 31) Console.WriteLine("ПОПАЛ!");
+            else Console.WriteLine("ПЕРЕЛЕТ");
+            Console.ReadKey();
+        }
+    }
+}
