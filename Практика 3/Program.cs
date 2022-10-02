7bec1561010ef0044056a2a436c57a386c51663f
Mrsuper125 <Mrsuper1221@gmail.com>
2022-10-02 19:11:20 +0300

    Практика 4

--- Практика 3/Program.cs
+++ Практика 3/Program.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace Практика_3
 {
+    //первые три сделал в тетради
     class Program
     {
+        public static string Ascending(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (b > c)
+                {
+                    return $"{c} {b} {a}";
+                }
+                else if (a > c)
+                {
+                    return $"{a} {c} {b}";
+                }
+                else return $"{c} {a} {b}";
+            }
+            else
+            {
+                if (a > c)
+                {
+                    return $"{b} {a} {c}";
+                }
+                else if (b > c)
+                {
+                    return $"{b} {c} {a}";
+                }
+                else return $"{c} {b} {a}";
+            }
+        }
+
+        // public static string boxes(int A1, int B1, int C1, int A2, int B2, int C2)
+        // {
+        // }
+        
         static void Main(string[] args)
         {
             int a = Convert.ToInt32(Console.ReadLine());
---
+++ Практика 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Практика_4
+{
+    internal class Program
+    {
+        public static double max(double a, double b, double c)
+        {
+            double max;
+            if (a > b) max = a;
+            else max = b;
+            if (c > max) max = c;
+            return max;
+        }
+
+        public static double min(double a, double b)
+        {
+            if (a < b) return a;
+            else return b;
+        }
+
+        public static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+            var res =
+                max(Math.Pow(min((a + 1) / a, b), 2.0), a, 2 / (a * b)) * min(a, max(0, -b, Math.Sqrt(1 + a * b))) +
+                2 * max(a, 3 + Math.Sqrt(a), Math.Pow(a, 3) * b);
+            Console.WriteLine(res);
+        }
+    }
+}
