69b487911d14e103b1425449b928de452c6ebe72
Mrsuper125 <Mrsuper1221@gmail.com>
2022-10-17 18:41:31 +0300

    Background 1, lesson 3.10

---
+++ Урок03.10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Урок03._10
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int min;
+            int max;
+            if (a < b)
+            {
+                min = a;
+                max = b;
+            }
+            else
+            {
+                min = b;
+                max = a;
+            }
+            Console.WriteLine($"{max/2 + max%2} {min}");
+        }
+    }
+}
---
+++ Фоновая 1.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Фоновая_1._1
+{
+    internal class Program
+    {
+        static double wrongU(double x, double y)
+        {
+            return x * Math.Pow(y, 3) + 7;
+        }
+        
+        public static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double U;
+            if (x<=0.5)
+            {
+                if (x>=-0.5)
+                {
+                    if (y<=1)
+                    {
+                        if (y>=-1)
+                        {
+                            U = y * Math.Pow(x, 2) - 1;
+                        }
+                        else
+                        {
+                            U = wrongU(x, y);
+                        }
+                    }
+                    else
+                    {
+                        U = wrongU(x, y);
+                    }
+                }
+                else
+                {
+                    U = wrongU(x, y);
+                }
+            }
+            else
+            {
+                U = wrongU(x, y);
+            }
+            Console.WriteLine(U);
+        }
+    }
+}
