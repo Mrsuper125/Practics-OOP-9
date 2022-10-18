91360a1b364bbeaccd76a53919064ec292112c78
Mrsuper125 <87068584+Mrsuper125@users.noreply.github.com>
2022-10-18 11:32:31 +0300

    fix Background 1.2 from School PC

--- Фоновая 1.2/Program.cs
+++ Фоновая 1.2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Фоновая_1._2
 {
@@ -7,10 +7,12 @@ namespace Фоновая_1._2
 
         public static int SecondMin(int N)
         {
+            Console.Write("Введите число ");
             int min1 = Int32.Parse(Console.ReadLine());
-            int min2 = min1;
+            int min2 = Int32.MaxValue;
             for (int i = 1; i < N; i++)
             {
+                Console.Write("Введите число ");
                 int cur = Int32.Parse(Console.ReadLine());
                 if (cur < min1)
                 {
@@ -28,6 +30,7 @@ namespace Фоновая_1._2
         
         public static void Main(string[] args)
         {
+            Console.Write("Введите количество чисел ");
             int N = int.Parse(Console.ReadLine());
             Console.WriteLine(SecondMin(N));
         }
