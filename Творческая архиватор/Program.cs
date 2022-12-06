12667b120001cc0703d1a534c04db6cf26c68453
Mrsuper125 <Mrsuper1221@gmail.com>
2022-12-06 22:00:57 +0300

    add empty project for creative task

---
+++ Творческая архиватор/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Творческая_архиватор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
