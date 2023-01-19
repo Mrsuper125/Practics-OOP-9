4052fe54257348b2192258ea6bc7564051f004d4
Mrsuper125 <Mrsuperr1221@gmail.com>
2023-01-19 15:06:54 +0300

    finished practice 12

--- Практика 12/Program.cs
+++ Практика 12/Program.cs
@@ -5,6 +5,50 @@ namespace Практика_12
     internal class Program
     {
 
+        public static int SumOffDigits(int n)
+        {
+            int res = 0;
+            while (n != 0)
+            {
+                res += n % 10;
+                n /= 10;
+            }
+
+            return res; 
+        }
+
+        static int MaxDigit(int n)
+        {
+            int res = 0;
+            while (n != 0)
+            {
+                if (n % 10 > res)
+                {
+                    res = n % 10;
+                }
+
+                res /= 10;
+            }
+
+            return res;
+        }
+
+        public static int Divisors(int n)
+        {
+            int res = 0;
+            for (int i = 0; i < Math.Sqrt(n); i++)
+            {
+                res += 2;
+            }
+
+            if ((int)Math.Pow(Math.Sqrt(n), 2) == n)
+            {
+                res += 1;
+            }
+
+            return res;
+        }
+
         public static int[] Create(int left, int right)
         {
             int len = right - left + 1;
@@ -31,15 +75,15 @@ namespace Практика_12
             return false;
         }
 
-        static int MinAndAmountOnInterval(out int min)
+        static int MinAndAmountOnInterval(int[] numbers, out int min)
         {
             min = 11201;
             int count = 0;
-            for (int i = 1200; i <= 11200; i+=5)
+            foreach (var item in numbers)
             {
-                if (!IsForbidden(i))
+                if (!IsForbidden(item) && item % 5 == 0)
                 {
-                    if (i < min) min = i;
+                    if (item < min) min = item;
                     count++;
                 }
             }
@@ -47,15 +91,15 @@ namespace Практика_12
             return count;
         }
         
-        static int MaxAndAmountOnInterval(out int max)
+        static int MaxAndAmountOnInterval(int[] numbers, out int max)
         {
             max = 3440;
             int count = 0;
-            for (int i = 3439; i <= 7410; i+=1)
+            foreach (var item in numbers)
             {
-                if ((i % 9 == 0 || i % 10 == 0 || i % 11 == 0) && (i%2 != i%6))
+                if ((item % 9 == 0 || item % 10 == 0 || item % 11 == 0) && (item%2 != item%6))
                 { 
-                    if (i > max) max = i;
+                    if (item > max) max = item;
                     count++;
                 }
             }
@@ -63,17 +107,17 @@ namespace Практика_12
             return count;
         }
         
-        static int AverageAndAmountOnInterval(out int average)
+        static int AverageAndAmountOnInterval(int[] numbers, out int average)
         {
             int max = 2475;
             int min = 7858;
             int count = 0;
-            for (int i = 2476; i <= 7857; i+=2)
+            foreach (var item in numbers)
             {
-                if (i%8 != 0 && i % 1000 / 100  <= 7)
+                if (item%8 != 0 && item % 2 == 0 && item % 1000 / 100  <= 7)
                 { 
-                    if (i > max) max = i;
-                    if (i < min) min = i;
+                    if (item > max) max = item;
+                    if (item < min) min = item;
                     count++;
                 }
             }
@@ -82,6 +126,43 @@ namespace Практика_12
 
             return count;
         }
+
+        static int DifferenceAndAmountOnInterval(int[] numbers, out int difference)
+        {
+            int min = 2224;
+            int max = 1213;
+            int count = 0;
+            foreach (var item in numbers)
+            {
+                if (SumOffDigits(item) == 14 && MaxDigit(item) == 7 && item % 2 == 0)
+                {
+                    if (item < min) min = item;
+                    if (item > max) max = item;
+                    count++;
+                }
+            }
+
+            difference = max - min;
+            return count;
+        }
+
+        static int MaxAndAmountOfNumbersWith15DivisorsOnInterval(int[] numbers, out int max)
+        {
+            int count = 0;
+            max = 123455;
+            foreach(var item in numbers)
+            {
+                if (Divisors(item) > 15)
+                {
+                    count++;
+                    if (item > max) max = item;
+                }
+            }
+
+            return count;
+        }
+        
+        // 1,2,3 задача от варианта 2. 4,5 - вариант 1.
         
         public static void Main(string[] args)
         {
