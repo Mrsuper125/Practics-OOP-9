8a277a0eea9ce117cf25a11efde6834283ce0a7a
Mrsuper125 <Mrsuperr1221@gmail.com>
2023-01-22 19:11:59 +0300

    Array algorithms

---
+++ Алгоритмы массивов/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Алгоритмы_массивов
+{
+    internal class Program
+    {
+
+        public static double AverageSquaredDifference(int[] mas, int N)
+        {
+            double average = 0;
+            foreach (var item in mas)
+            {
+                average += item;
+            }
+
+            average /= N;
+
+            double dispersion = 0;
+
+            foreach (var item in mas)
+            {
+                dispersion += Math.Pow(item - average, 2);
+            }
+
+            dispersion /= N;
+
+            return Math.Sqrt(dispersion);
+        }
+        
+        public static void Main(string[] args)
+        {
+            int[] arr = {1, 2, 3, 4, 5};
+            int N = arr.Length;
+            Console.WriteLine(AverageSquaredDifference(arr, N));
+        }
+    }
+}
