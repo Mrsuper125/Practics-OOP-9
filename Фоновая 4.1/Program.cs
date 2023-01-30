94e4df2478668f133eafb9c40995cdb6271b3007
Mrsuper125 <Mrsuperr1221@gmail.com>
2023-01-30 19:06:58 +0300

    Background 4 part 1

---
+++ Фоновая 4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Фоновая_4._1
+{
+    internal class Program
+    {
+        public static int[] Input(int N)
+        {
+            int[] res = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                res[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return res;
+        }
+
+        public static void Print(int[] arr)
+        {
+            foreach (int elem in arr)
+            {
+                Console.Write(elem + " ");
+            }
+        }
+        
+        
+        
+        public static void Main(string[] args)
+        {
+            int[]arr = Input(10);
+            Print(arr);
+        }
+    }
+}
