4ad1fce0ed252b0617b457d1c46a1e8700de1e6e
Mrsuper125 <Mrsuper1221@gmail.com>
2023-04-18 18:19:26 +0300

    Card games finished

--- Игры с картами/Program.cs
+++ Игры с картами/Program.cs
@@ -1,201 +1,195 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Игры_с_картами
 {
-    class List
+    public enum Cards
     {
-        private ListItem head;
+        Шестёрка = 0,
+        Семёрка,
+        Восьмёрка,
+        Девятка,
+        Десятка,
+        Валет,
+        Королева,
+        Король,
+        Туз
+    }
 
-        public List()
-        {
-            head = null;
-        }
+    class Deck
+    {
+        private List<Cards> deck;
 
-        public void Append(int value)
+        public Deck()
         {
-            if (head == null)
+            deck = new List<Cards>();
+            for (int i = 0; i < 4; i++)
             {
-                head = new ListItem(value, null);
-            }
-            else
-            {
-                ListItem current = head;
-                while (current.next != null)
+                for (int j = (int)Cards.Шестёрка; j <= (int)Cards.Туз; j++)
                 {
-                    current = current.next;
+                    deck.Add((Cards)j);
                 }
-                current.next = new ListItem(value, null);
-            }
-        }
-        
-        public List(int length)
-        {
-            for (int i = 0; i < length; i++)
-            {
-                Append(0);
             }
         }
 
-        public int Length
+        public Cards this[int position]
         {
             get
             {
-                if (head == null)
-                {
-                    return 0;
-                }
-                else
-                {
-                    int res = 1;
-                    ListItem link = head;
-                    while (link.next != null)
-                    {
-
-                        link = link.next;
-                        res++;
-                    }
-
-                    return res;
-                }
-            }
-        }
-        
-        private ListItem GetItem(int index)
-        {
-            if(index >= 0 && index < Length)
-            {
-                ListItem current = head;
-                while (index > 0)
-                {
-                    current = current.next;
-                    index--;
-                }
-
-                return current;
-            }
-            else
-            {
-                throw new ArgumentException("List index out of range");
-            }
-        }
-        
-        private void SetItem(int index, int value)
-        {
-            int i = index;
-            if(index > 0 && index < Length)
-            {
-                ListItem current = head;
-                while (i > 0)
-                {
-                    current = current.next;
-                    i--;
-                }
-
-                
-                current.value = value;
-            }
-            else if (index == 0)
-            {
-                head.value = value;
-            }
-            else
-            {
-                throw new ArgumentException("List index out of range");
-            }
-        }
-        
-        public int this[int index]
-        {
-            get
-            {
-                if(index >= 0 && index < Length)
-                {
-                    ListItem current = head;
-                    while (index > 0)
-                    {
-                        current = current.next;
-                        index--;
-                    }
-
-                    return current.value;
-                }
-                else
-                {
-                    throw new ArgumentException("List index out of range");
-                }
-            }
-            set
-            {
-                if(index >= 0 && index < Length)
-                {
-                    ListItem current = head;
-                    while (index > 0)
-                    {
-                        current = current.next;
-                        index--;
-                    }
-
-                    // if (value  == null)
-                    // {
-                    //     current = null;
-                    // }
-                    // else
-                    // {
-                        current.value = value;
-                        
-                    // }
-                }
-                else
-                {
-                    throw new ArgumentException("List index out of range");
-                }
+                return deck[position-1];
             }
         }
 
-        public void Pop(int index)
+        public void Pop(int position)
         {
-            if (Length == 0)
+            deck.RemoveAt(position-1);
+        }
+
+        public void Shuffle()
+        {
+            List<Cards> auxillary = new List<Cards>();
+            foreach (Cards item in deck)
             {
-                throw new ArgumentException("Can't pop items from an empty List");
+                auxillary.Add(item);
             }
-            else
+
+            deck = new List<Cards>();
+            int len = auxillary.Count;
+            Random rn = new Random();
+            for (int i = 0; i < len; i++)
             {
-                if (index >= Length || index < 0)
-                {
-                    throw new ArgumentException("List index out of range");
-                }
-                else
-                {
-                    if (Length  == index + 1)
-                    {
-                        if (index == 0)
-                        {
-                            this.head = null;
-                        }
-                        else
-                        {
-                            GetItem(index-1).next = null;
-                        }
-                    }
-                    else
-                    {
-                        SetItem(index, this[index + 1]);
-                        GetItem(index).next = GetItem(index+1).next;
-                    }
-                }
+                int index = rn.Next(0, auxillary.Count);
+                deck.Add(auxillary[index]);
+                auxillary.RemoveAt(index);
             }
+        }
+
+        public void Give(Player player, int amount)
+        {
+            List<Cards> toGive;
+            toGive = deck.GetRange(0, amount);
+            player.Get(toGive);
+            deck.RemoveRange(0, amount);
         }
     }
 
-    class ListItem
+    public class Player
     {
-        public ListItem next;
-        public int value;
+        private List<Cards> deck;
+        public List<Cards> openedDeck;
+        public int number;
 
-        public ListItem(int value, ListItem next)
+        public Player(int number)
         {
-            this.value = value;
-            this.next = next;
+            deck = new List<Cards>();
+            openedDeck = new List<Cards>();
+            this.number = number;
+        }
+
+        public void Get(List<Cards> cards)
+        {
+            openedDeck = new List<Cards>();
+            deck.AddRange(cards);
+            //Console.WriteLine($"Игрок {number} получил {cards.Count} карт");
+        }
+
+        public void Open(int amount)
+        {
+            if (deck.Count < amount)
+            {
+                Console.WriteLine($"У игрока {number} недостаточно карт. Игра окончена. Победил игрок {3 - number}.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                openedDeck.AddRange(deck.GetRange(0, amount));
+                deck.RemoveRange(0, amount);
+            }
+        }
+
+        public static List<Cards> Compose(List<Cards> list1, List<Cards> list2)
+        {
+            List<Cards> list1Copy = new List<Cards>(list1);
+            List<Cards> list2Copy = new List<Cards>(list2);
+            int minLength = Math.Min(list1Copy.Count, list2Copy.Count);
+            list1Copy = list1Copy.GetRange(0, minLength);
+            list2Copy = list2Copy.GetRange(0, minLength);
+            List<Cards> res = new List<Cards>();
+            res.Add(list1[0]);
+            res.Add(list2[0]);
+            list1Copy.RemoveAt(0);
+            list2Copy.RemoveAt(0);
+            while (list1Copy.Count != 0)
+            {
+                res.AddRange(list1Copy.GetRange(0, 2));
+                list1Copy.RemoveRange(0, 2);
+                res.AddRange(list2Copy.GetRange(0, 2));
+                list2Copy.RemoveRange(0, 2);
+            }
+            return res;
+        }
+        
+        /*public static List<Cards> Compose(List<Cards> list1, List<Cards> list2)
+        {
+            List<Cards> res = new List<Cards>();
+            res.Add(list1[0]);
+            res.Add(list2[0]);
+            list1.RemoveAt(0);
+            list2.RemoveAt(0);
+            while (list1.Count != 0)
+            {
+                res.AddRange(list1.GetRange(0, 2));
+                list1.RemoveRange(0, 2);
+                res.AddRange(list2.GetRange(0, 2));
+                list2.RemoveRange(0, 2);
+            }
+            return res;
+        }*/
+        
+        public static void Compare(Player player1, Player player2)
+        {
+            player1.Open(1);
+            Console.WriteLine("Первый игрок вскрывает карту, и это " + player1.openedDeck[0] + ".");
+            player2.Open(1);
+            Console.WriteLine("Второй игрок вскрывает карту, и это " + player2.openedDeck[0] + ".");
+            if (player1.openedDeck.Last() > player2.openedDeck.Last())
+            {
+                player1.Get(Compose(player1.openedDeck, player2.openedDeck));
+                Console.WriteLine("Первый игрок победил, и забирает обе карты.");
+                player2.openedDeck = new List<Cards>();
+            }
+            else if (player1.openedDeck.Last() < player2.openedDeck.Last())
+            {
+                player2.Get(Compose(player1.openedDeck, player2.openedDeck));
+                Console.WriteLine("Второй игрок победил, и забирает обе карты.");
+                player1.openedDeck = new List<Cards>();
+            }
+            else
+            {
+                Console.WriteLine("Началась война.");
+                while (player1.openedDeck.Last() == player2.openedDeck.Last())
+                {
+                    player1.Open(2);
+                    Console.WriteLine("Первый игрок вскрывает карту, и это " + player1.openedDeck.Last() + ".");
+                    player2.Open(2);
+                    Console.WriteLine("Второй игрок вскрывает карту, и это " + player2.openedDeck.Last() + ".");
+                }
+                if (player1.openedDeck.Last() > player2.openedDeck.Last())
+                { 
+                    Console.WriteLine("Первый игрок победил, и забирает " + (player1.openedDeck.Count*2) +" карт.");
+                    player1.Get(Compose(player1.openedDeck, player2.openedDeck));
+                    player2.openedDeck = new List<Cards>();
+                }
+                else if (player1.openedDeck.Last() < player2.openedDeck.Last())
+                {
+                    Console.WriteLine("Второй игрок победил, и забирает " + (player1.openedDeck.Count*2) +" карт.");
+                    player2.Get(Compose(player1.openedDeck, player2.openedDeck));
+                    player1.openedDeck = new List<Cards>();
+                }
+            }
+            Console.WriteLine(player1.deck.Count + player2.deck.Count);
+            Console.WriteLine(player1.openedDeck.Count + player2.openedDeck.Count);
         }
     }
     
@@ -203,19 +197,38 @@ namespace Игры_с_картами
     {
         public static void Main(string[] args)
         {
-            List list = new List(3);
-            for (int i = 0; i < list.Length; i++)
+            Deck deck = new Deck();
+            deck.Shuffle();
+            Player player1 = new Player(1);
+            Player player2 = new Player(2);
+            
+            deck.Give(player1, 18);
+            deck.Give(player2, 18);
+
+            while (true)
             {
-                Console.WriteLine(list[i]);
-            }
-            Console.WriteLine(list.Length);
-            list[1] = 1;
-            list[2] = 2;
-            list.Pop(1);
-            for (int i = 0; i < list.Length; i++)
-            {
-                Console.WriteLine(list[i]);
+                Player.Compare(player1, player2);
             }
         }
     }
+    
+    // internal class Program
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         List list = new List(3);
+    //         for (int i = 0; i < list.Length; i++)
+    //         {
+    //             Console.WriteLine(list[i]);
+    //         }
+    //         Console.WriteLine(list.Length);
+    //         list[1] = 1;
+    //         list[2] = 2;
+    //         list.Pop(1);
+    //         for (int i = 0; i < list.Length; i++)
+    //         {
+    //             Console.WriteLine(list[i]);
+    //         }
+    //     }
+    // }
 }
